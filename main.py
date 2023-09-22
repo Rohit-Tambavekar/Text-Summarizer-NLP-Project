eee47f713b55832f65e71e60b1d9965267b17cf3
Rohit Tambavekar <129712399+Rohit-Tambavekar@users.noreply.github.com>
2023-09-23 01:44:36 +0530

    data validation complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -6,6 +7,16 @@ try:
     logger.info(f">>>>>> Stage{STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> Stage{STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,7 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
-from textSummarizer.entity import (DataIngestionConfig)
+from textSummarizer.entity import (DataIngestionConfig, DataValidationConfig)
+
 
 
 class ConfigurationManager:
@@ -25,5 +26,17 @@ class ConfigurationManager:
             local_data_file=config.local_data_file,
             unzip_dir=config.unzip_dir 
         )
-
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
