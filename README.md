c233cdc8f9821cd004e2c8dddb4aa53d87d6a4d7
Rohit Tambavekar <129712399+Rohit-Tambavekar@users.noreply.github.com>
2023-09-23 01:01:43 +0530

    all changes till data ingestion

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Text-Summarizer-NLP-Project
+# End to end Text-Summarizer-NLP-Project
 This code will help you in summarizing you text
+
+## Workflows
+
+1. Update config.yaml
+2. Update param.yaml
+3. Update entity
+4. Update Configuration Manager in src/config/configuration.py
+5. Update components
+6. Update pipeline
+7. update the main.py
+8. Update app.py
+
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("Welcome to our custom log")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> Stage{STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
